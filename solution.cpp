0ce349148cdf11be68a6ef3990b4d98e68cbc499
Kooreis <kooreis.official@gmail.com>
2023-03-30 21:05:11 -0500

    Created a function to create a new node. It takes an integer as input, creates a new node, assigns the input data to the node, and returns the node.

--- solution.cpp
+++ solution.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
-
-struct Node {
-    int data;
-    Node* next;
-};
+Node* createNode(int data) {
+    Node* newNode = new Node();
+    if (!newNode) {
+        std::cout << "Memory error\n";
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
