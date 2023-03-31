1e111dc277926f18ddb8881fd4c4f3f9ea39c436
Kooreis <kooreis.official@gmail.com>
2023-03-30 21:05:11 -0500

    This update introduces the LinkedList class and the insertAtEnd method. The LinkedList class has a head property that points to the first node in the list. The insertAtEnd method creates a new node with the provided data and adds it to the end of the list.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,18 @@
-class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertAtEnd(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
 }
